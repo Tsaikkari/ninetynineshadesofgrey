5786c3b4949c27ccdda9980daf549c6a44496326
Tsaikkari <trospe@gmx.de>
2020-03-24 21:37:13 +0100

    Adding animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// greys grid
+// user clicks button and the divs move
+$('#move').on('click', function() {
+    $('.greys-grid-screen').css('display', 'none');
+    $('.black, .white').addClass('animated').addClass('moved');
+    setTimeout(function() {
+        $('.black, .white').removeClass('animated').removeClass('moved');
+    }, 1000);
+    $('.greys-grid-screen').css('display', 'block'); 
+    //$('.two-divs-screen').css('display', 'none');
+});
+
+
+
+
+
+
+
