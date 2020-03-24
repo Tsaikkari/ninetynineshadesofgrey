fba014296bcc34b5e9a24f7e4f65337370c51a77
Tsaikkari <trospe@gmx.de>
2020-03-24 22:45:14 +0100

    Adding nextScreen function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 // user clicks button and the divs move
 $('#move').on('click', function() {
-    $('.greys-grid-screen').css('display', 'none');
-    $('.black, .white').addClass('animated').addClass('moved');
-    setTimeout(function() {
-        $('.black, .white').removeClass('animated').removeClass('moved');
-    }, 1000);
-    $('.greys-grid-screen').css('display', 'block'); 
-    //$('.two-divs-screen').css('display', 'none');
+  $('.greys-grid-screen').css('display', 'none');
+  $('.black, .white').addClass('animated').addClass('moved');
+  setTimeout(function() {
+    $('.black, .white').removeClass('animated').removeClass('moved');
+  }, 1000);
+  //isMoved = true;
+  setTimeout(function() {
+    nextScreen();
+  }, 1000);
 });
+
+function nextScreen() {
+  $('h2').text("Learn about colorful people");
+  $('.greys-grid-screen').css('display', 'block').css('background-color', 'grey'); // placeholder
+}
+
+
+
+ 
+
+
 
 
 
