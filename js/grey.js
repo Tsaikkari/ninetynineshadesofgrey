84c77cdc44bc782aa573b29e753a78c1b469a47f
Tsaikkari <trospe@gmx.de>
2020-03-26 23:57:19 +0100

    Add all buttons

--- js/grey.js
+++ js/grey.js
@@ -38,6 +38,461 @@ let greys = [
         href: "/link/to/page8",
         name: "",
         backgroundColor: ""
+    },
+    {
+        href: "/link/to/page9",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page10",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page11",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page12",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page13",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page14",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page15",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page16",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page17",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page18",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page19",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page20",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page21",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page22",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page23",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page24",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page25",
+        name: "",
+        backgroundColor: "030303"
+    },
+    {
+        href: "/link/to/page26",
+        name: "",
+        backgroundColor: "000"
+    },
+    {
+        href: "/link/to/page27",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page28",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page29",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page30",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page31",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page32",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page33",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page34",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page35",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page36",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page37",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page38",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page39",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page40",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page1",
+        name: "Wolfgang Amadeus Mozart",
+        backgroundColor: "030303"
+    },
+    {
+        href: "/link/to/page2",
+        name: "",
+        backgroundColor: "000"
+    },
+    {
+        href: "/link/to/page3",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page4",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page5",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page6",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page7",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page1",
+        name: "Wolfgang Amadeus Mozart",
+        backgroundColor: "030303"
+    },
+    {
+        href: "/link/to/page2",
+        name: "",
+        backgroundColor: "000"
+    },
+    {
+        href: "/link/to/page3",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page4",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page5",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page6",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page7",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page8",
+        name: "",
+        backgroundColor: ""
+    },
+    {
+        href: "/link/to/page1",
+        name: "Wolfgang Amadeus Mozart",
+        backgroundColor: "030303"
+    },
+    {
+        href: "/link/to/page2",
+        name: "",
+        backgroundColor: "000"
+    },
+    {
+        href: "/link/to/page3",
+        name: "",
+        backgroundColor: ""
     }
 ];
 
