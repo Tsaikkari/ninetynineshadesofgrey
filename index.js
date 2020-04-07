3bd9e6d8c44932cf08de9f190f5684b8bff480eb
Tsaikkari <trospe@gmx.de>
2020-04-07 20:21:36 +0200

    Adding a posts array

--- index.js
+++ index.js
@@ -15,12 +15,14 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
+let posts = [];
+
 app.get('/', (req, res) => {
   res.render('index');
 }); 
 
 app.get('/profile', (req, res) => {
-  res.render('profile');
+  res.render('profile', {posts: posts});
 });
 
 app.get('/compose', (req, res) => {
@@ -29,6 +31,12 @@ app.get('/compose', (req, res) => {
 
 app.post('/compose', (req, res) => {
   console.log(req.body.profileTitle);
+  const post = {
+    personName: req.body.profileTitle,
+    description: req.body.descriptionBody
+  };
+  posts.push(post);
+  res.redirect('/profile');
 });
 
 app.listen(3001, () => {
